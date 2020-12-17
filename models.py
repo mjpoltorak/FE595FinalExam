2e152e963187d04fda6e1271abd462b8e1a8e7f3
AGalli98 <45046175+AGalli98@users.noreply.github.com>
2020-12-16 22:04:25 -0500

    alex

--- models.py
+++ models.py
@@ -4,7 +4,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
 
 data = pd.read_csv("subset_cleaned_election_data.csv")
 data = data.dropna()
@@ -33,3 +35,30 @@ filename = 'svm.pkl'
 svm_pkl = open(filename, 'wb')
 pickle.dump(clf, svm_pkl)
 svm_pkl.close()
+
+#K Nearest Neighbors
+n_neighbors = 7
+knnmodel = KNeighborsClassifier(n_neighbors)
+knn = knnmodel.fit(x_train, y_train)
+prediction = knn.predict(x_test)
+print(accuracy_score(y_test,prediction))
+
+filename = 'knn.pkl'
+knn_pkl = open(filename, 'wb')
+pickle.dump(knn, knn_pkl)
+knn_pkl.close()
+
+#Random Forest
+n_estimators = 100
+rfmodel = RandomForestClassifier(n_estimators, max_depth = 5)
+rf = rfmodel.fit(x_train, y_train)
+prediction = rf.predict(x_test)
+print(accuracy_score(y_test,prediction))
+
+filename = 'rf.pkl'
+rf_pkl = open(filename, 'wb')
+pickle.dump(rf, rf_pkl)
+rf_pkl.close()
+
+
+
